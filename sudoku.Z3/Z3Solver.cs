40d1e0671a737a5f29626f61f398f77a107e42ab
Remi <remi.beltramini@gmail.com>
2020-10-09 14:34:03 +0200

    Initialization

---
+++ sudoku.Z3/Z3Solver.cs
@@ -0,0 +1,14 @@
+﻿using Sudoku.Core;
+using System;
+
+namespace sudoku.Z3
+{
+
+    public class Z3Solver : ISudokuSolver
+    {
+        public Sudoku.Core.Sudoku Solve(Sudoku.Core.Sudoku s)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
