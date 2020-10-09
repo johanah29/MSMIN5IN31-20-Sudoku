cec42ec2a6dcd67b58160becb8358457a0814e5f
unknown <marthe_de_p@hotmail.fr>
2020-10-09 16:08:22 +0200

    recup force brut du prof

--- Sudoku.Benchmark/Program.cs
+++ Sudoku.Benchmark/Program.cs
@@ -18,45 +18,52 @@ namespace Sudoku.Benchmark
                 var solvers = GetSolvers();
 
                 Console.WriteLine("Benchmarking Sudoku Solvers");
-                Console.WriteLine("Select difficulty: 1-Easy, 2-Medium, 3-Hard");
-                var strDiff = Console.ReadLine();
-                int intDiff;
-                int.TryParse(strDiff, out intDiff);
-                string fileName;
-                switch (intDiff)
+
+                while (true)
                 {
-                    case 1:
-                        fileName = "Sudoku_Easy50.txt";
-                        break;
-                    case 2:
-                        fileName = "Sudoku_hardest.txt";
-                        break;
-                    default:
-                        fileName = "Sudoku_top95.txt";
-                        break;
+                    Console.WriteLine("Select difficulty: 1-Easy, 2-Medium, 3-Hard");
+                    var strDiff = Console.ReadLine();
+                    int intDiff;
+                    int.TryParse(strDiff, out intDiff);
+                    string fileName;
+                    switch (intDiff)
+                    {
+                        case 1:
+                            fileName = "Sudoku_Easy50.txt";
+                            break;
+                        case 2:
+                            fileName = "Sudoku_hardest.txt";
+                            break;
+                        default:
+                            fileName = "Sudoku_top95.txt";
+                            break;
+                    }
+
+                    string filePath = $@"..\..\..\..\Samples\{fileName}";
+                    var sudokus = Core.Sudoku.ParseFile(filePath);
+
+                    Console.WriteLine("Choose a solver:");
+                    for (int i = 0; i < solvers.Count(); i++)
+                    {
+                        Console.WriteLine($"{(i + 1).ToString(CultureInfo.InvariantCulture)} - {solvers[i].GetType().FullName}");
+                    }
+                    var strSolver = Console.ReadLine();
+                    int intSolver;
+                    int.TryParse(strSolver, out intSolver);
+                    var solver = solvers[intSolver - 1];
+                    var targetSudoku = sudokus[0];
+                    Console.WriteLine("Sudoku à résoudre:");
+                    Console.WriteLine(targetSudoku.ToString());
+                    var sw = Stopwatch.StartNew();
+
+                    var solution = solver.Solve(targetSudoku);
+
+                    var elapsed = sw.Elapsed;
+                    Console.WriteLine("solution:");
+                    Console.WriteLine(solution.ToString());
+                    Console.WriteLine($"Time to solution: {elapsed.TotalMilliseconds} ms");
                 }
-
-                string filePath = $@"..\..\..\..\Samples\{fileName}";
-                var sudokus = Core.Sudoku.ParseFile(filePath);
-
-                Console.WriteLine("Choose a solver:");
-                for (int i = 0; i < solvers.Count(); i++)
-                {
-                    Console.WriteLine($"{(i + 1).ToString(CultureInfo.InvariantCulture)} - {solvers[i].GetType().FullName}");
-                }
-                var strSolver = Console.ReadLine();
-                int intSolver;
-                int.TryParse(strDiff, out intSolver);
-                var solver = solvers[intSolver - 1];
-
-                var sw = Stopwatch.StartNew();
-
-                var solution = solver.Solve(sudokus[0]);
-
-                var elapsed = sw.Elapsed;
-                Console.WriteLine("solution:");
-                Console.WriteLine(solution.ToString());
-                Console.WriteLine($"Time to solution: {elapsed.TotalMilliseconds} ms");
+              
             }
             catch (Exception e)
             {
