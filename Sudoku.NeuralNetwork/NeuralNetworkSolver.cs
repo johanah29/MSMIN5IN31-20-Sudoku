db1276a2c1cbd2e9945bbf74eccc98b667d92698
55750061 <delattre.hugo@gmail.com>
2020-10-20 08:59:43 +0200

    Create project

---
+++ Sudoku.NeuralNetwork/NeuralNetworkSolver.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Sudoku.Core;
+
+
+namespace Sudoku.NeuralNetwork
+{
+    public class NeuralNetworkSolver : ISudokuSolver
+    {
+        public Core.Sudoku Solve(Core.Sudoku s)
+        {
+            return s;
+        }
+    }
+}
+
